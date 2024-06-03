ca7b285bf5829c6582455be8232a7213bce88ffe
José Eduardo (zeduardu) <zeduardu@gmail.com>
2024-06-03 05:10:25 -0300

    refactor: disable h2 configuration

---
+++ api/src/main/java/dev/arch420x0/archce/infrastructure/configuration/H2ServerConfiguration.java
@@ -0,0 +1,17 @@
+package dev.arch420x0.archce.infrastructure.configuration;
+
+//import org.h2.tools.Server;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//import java.sql.SQLException;
+
+//@Component
+public class H2ServerConfiguration {
+//  @Bean(initMethod = "start", destroyMethod = "shutdown")
+//  public Server h2Server() throws SQLException {
+//    return Server.createTcpServer(
+//      "-tcpPort", "9090", "-tcpAllowOthers"
+//    );
+//  }
+}
