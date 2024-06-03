19b020768f5f54b721d8f0e714a462b85704b469
José Eduardo (zeduardu) <zeduardu@gmail.com>
2024-06-03 05:21:40 -0300

    feat: add template functionality

--- clientapp/src/app/app-routing.module.ts
+++ clientapp/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
 import { ElaborationComponent } from './modules/elaboration/elaboration.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { HomeComponent } from '@modules/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'elaboration', component: ElaborationComponent}
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'elaboration', component: ElaborationComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
