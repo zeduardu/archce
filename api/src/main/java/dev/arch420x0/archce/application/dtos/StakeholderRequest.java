69bd9e3f3d203d52c41ec83b9055e3d0983d70eb
José Eduardo (zeduardu) <zeduardu@gmail.com>
2024-07-12 05:49:47 -0300

    wip: many changes in progress

--- api/src/main/java/dev/arch420x0/archce/application/dtos/StakeholderRequest.java
+++ api/src/main/java/dev/arch420x0/archce/application/dtos/StakeholderRequest.java
@@ -1,22 +1,2 @@
-package dev.arch420x0.archce.application.dtos;
-
-import dev.arch420x0.archce.domain.entities.EntityInterest;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
-import lombok.Value;
-
-import java.io.Serializable;
-
-/**
- * DTO for {@link dev.arch420x0.archce.domain.entities.Stakeholder}
- */
-@Data
-public class StakeholderRequest implements Serializable {
-  Long id;
-  @NotNull(message = "Name can't be null")
-  @NotBlank(message = "Name can't be empty")
-  String nome;
-  String type;
-  EntityInterestRequest entityInterest;
+package dev.arch420x0.archce.application.dtos;public class StakeholderRequest {
 }
---
+++ api/src/main/java/dev/arch420x0/archce/domain/entities/View.java
@@ -0,0 +1,2 @@
+package dev.arch420x0.archce.domain.entities;public class View {
+}
